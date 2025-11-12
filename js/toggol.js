aef651bea4efcb0a6af6bb1eb74cdf092ef3dc6b
imtiazramin <imtiazramin7@gmail.com>
2025-11-12 20:49:52 +0600

    Logout Section Done

---
+++ js/toggol.js
@@ -0,0 +1,61 @@
+ document.getElementById("bannar-contanier").style.display="block"
+
+document.getElementById("FAQ-btn").addEventListener("click",function(event){
+    document.getElementById("Vocabularies-contanier").style.display="block"
+    
+    document.getElementById("bannar-contanier").style.display="none"
+
+})
+
+
+document.getElementById("Learn-btn").addEventListener("click",function(event){
+    document.getElementById("Questions-contanier").style.display="block"
+    document.getElementById("Vocabularies-contanier").style.display="none"
+    document.getElementById("bannar-contanier").style.display="none"
+
+})
+
+
+
+
+
+document.getElementById("main-contanier").style.display="none"
+ document.getElementById("get-btn").addEventListener("click",function(event){
+    event.preventDefault()
+   
+    const personeName=document.getElementById("name").value;
+
+    const personePassword=document.getElementById("pin").value;
+ 
+   const convertedName="test"
+   const convertedPin="123456"
+
+    if(personeName===convertedName){
+
+        if(personePassword===convertedPin){
+             document.getElementById("main-contanier").style.display="block"
+            
+        }
+        else{
+           alert("Show an alert if the password is not 123456")
+    } 
+    }
+    else{
+       alert("Show an alert if the user does not enter a name")
+    }
+})
+
+
+
+document.getElementById("Questions-contanier").style.display="block"
+    document.getElementById("Vocabularies-contanier").style.display="block"
+    document.getElementById("bannar-contanier").style.display="block"
+document.getElementById("Logout-btn").addEventListener("click",function(){
+    document.getElementById("Questions-contanier").style.display="none"
+    document.getElementById("Vocabularies-contanier").style.display="none"
+    document.getElementById("bannar-contanier").style.display="block"
+    document.getElementById("nav-contanier").style.display="none"
+
+})
+
+
